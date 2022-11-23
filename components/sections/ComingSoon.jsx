f38de1adf4aef9444b84f79fa7e35ffc01c1eb5d
samuel-dean-machine <sam.pauldean@gmail.com>
2022-11-22 21:54:43 -0600

    Add logo

--- components/sections/ComingSoon.jsx
+++ components/sections/ComingSoon.jsx
@@ -4,14 +4,34 @@ import Animate from '../atoms/Animate'
 import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { useRef, useEffect } from 'react'
+import { gsap } from 'gsap'
 
 import style from "./ComingSoon.module.scss"
 
 const ComingSoon = (props) => {
+  const myRef = useRef(null);
+
+  useEffect(() => {
+    const el = myRef.current;
+
+    gsap.to(el, {rotation:"360", duration: 4, ease: 'none', repeat:-1});
+  }, [])
+
   return (
     <Animate>
       <section className={style.comingSoon}>
         <div className={style.comingSoon__container}>
+          {props.logo && (
+            <div ref={myRef} className={style.comingSoon__logo}>
+              <Image
+                src={props.logo}
+                alt="Logo"
+                width={100}
+                height={100}
+              />
+            </div>
+          )}
           {props.title && (
             <h1 className={`${style.comingSoon__title} animate animate--fade-up`}>{props.title}</h1>
           )}
@@ -40,6 +60,11 @@ const ComingSoon = (props) => {
 Builder.registerComponent(ComingSoon, {
   name: 'Coming Soon',
   inputs: [
+    {
+      name: 'logo',
+      type: 'file',
+      allowedFileTypes: ['svg', 'png', 'jpg', 'jpeg'],
+    },
     {
       name: 'title',
       type: 'string',
