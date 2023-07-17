b75003a97a89b9cd3eec0dbeb9eea2143c2a22c7
samuel-dean-machine <sam.pauldean@gmail.com>
2023-07-17 10:45:43 -0500

    De-structured product component

--- components/molecules/ProductCard.jsx
+++ components/molecules/ProductCard.jsx
@@ -3,20 +3,20 @@ import Image from 'next/image'
 
 import * as style from './ProductCard.module.scss'
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product: { node: product } }) => {
 
   console.log('PRODUCT:', product)
 
   return (
-    <div className={style.Products__card} key={product.node.id}>
+    <div className={style.Products__card} key={product.id}>
       <div className={style.Products__card_text_container}>
-        <h3 className={style.Products__card_title}>{product.node.title}</h3>
-        <p className={style.Products__card_description}>{product.node.description}</p>
+        <h3 className={style.Products__card_title}>{product.title}</h3>
+        <p className={style.Products__card_description}>{product.description}</p>
       </div>
       <div className={style.Products__card_image_container}>
         <Image
-          src={product.node.featuredImage.url}
-          alt={product.node.featuredImage.altText}
+          src={product.featuredImage.url}
+          alt={product.featuredImage.altText}
           fill
         />
       </div>
