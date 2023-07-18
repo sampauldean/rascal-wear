d487eac21a85bea51c4628974fd7e12f9a3475ae
samuel-dean-machine <sam.pauldean@gmail.com>
2023-07-18 14:51:58 -0500

    Revert "Maybe a cart?"
    This reverts commit 1a3f4023d7a46c946c8d33e6b281afd9c21da8df.

--- components/molecules/ProductCard.jsx
+++ components/molecules/ProductCard.jsx
@@ -1,13 +1,16 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import {useCart} from '../../context/cart'
+
 import * as style from './ProductCard.module.scss'
 
 const ProductCard = ({ product: { node: product } }) => {
-  const {addToCart} = useCart()
 
   return (
-    <div className={style.Products__card} key={product.id}>
+    <Link
+      href={`/shop/${product.handle}`}
+      className={style.Products__card} 
+      key={product.id}
+    >
       <div className={style.Products__card_text_container}>
         <h3 className={style.Products__card_title}>{product.title}</h3>
         <p className={style.Products__card_description}>{product.description}</p>
@@ -19,10 +22,8 @@ const ProductCard = ({ product: { node: product } }) => {
           fill
         />
       </div>
-      <button onClick={() => addToCart(product.id, 1)}>Add to Cart</button>
-      <Link href={`/shop/${product.handle}`}>View Details</Link>
-    </div>
-  )
+    </Link>
+)
 }
 
 export default ProductCard
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import '../styles/normalize.css'
 import '../styles/global.scss'
-import {CartProvider} from '../context/cart'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <CartProvider>
-        <Component {...pageProps} />
-      </CartProvider>
+      <Component {...pageProps} />
     </>
   )
 }
